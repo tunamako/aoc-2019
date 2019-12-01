dd1fab5783d6365725f0f4abbb59fefebcd41071
tunamako <inedibledelicacies@gmail.com>
2019-11-30 21:58:15 -0600

    create template

---
+++ template.py
@@ -0,0 +1,18 @@
+from collections import defaultdict, namedtuple
+from itertools import permutations, combinations, chain
+
+
+def part_one(_input):
+    pass
+
+
+def part_two(_input):
+    pass
+
+
+if __name__ == '__main__':
+	_input = open("input", 'r').readlines()
+
+
+    print(part_one(_input))
+    print(part_two(_input))
